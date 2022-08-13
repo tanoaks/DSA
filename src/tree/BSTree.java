4ae31b0ce4d35a547225bb32149a853464d64743
tanoaks <mai@infy155>
2022-08-13 23:05:40 +0530

    BST implemention

--- src/tree/BSTree.java
+++ src/tree/BSTree.java
@@ -9,11 +9,11 @@ public class BSTree<T extends Comparable<T>> {
 
     private Node<T> root;
 
-    Node<T> add(T value) {
+    public Node<T> add(T value) {
         return add(value, root);
     }
 
-    Node<T> add(T value, Node<T> parent) {
+    private Node<T> add(T value, Node<T> parent) {
         Node<T> newNode;
         // in case no root node
         if (this.root == null) {
@@ -39,26 +39,67 @@ public class BSTree<T extends Comparable<T>> {
 
 
     List<Node<T>> listAll() {
-        List<Node<T>> list= new  ArrayList();
-        return listAll(root,list) ;
+        List<Node<T>> list = new ArrayList();
+        return listAll(root, list);
     }
 
     private List<Node<T>> listAll(Node<T> root, List<Node<T>> list) {
-        if (root==null){
+        if (root == null) {
             return list;
         }
-        listAll(root.getLeft(),list);
+        listAll(root.getLeft(), list);
         list.add(root);
-        listAll(root.getRight(),list);
-    return list;
+        listAll(root.getRight(), list);
+        return list;
     }
 
-    Node<T> rotate() {
-        return null;
+    public void rotate() {
+        root = rotate(root);
     }
 
-    int depth() {
-        return -1;
+    public Node<T> rotate(Node<T> node) {
+        if (node == null)
+            return null;
+        Node<T> temp = rotate(node.getLeft());
+        node.setLeft(rotate(node.getRight()));
+        node.setRight(temp);
+        return node;
     }
+
+    public int depth() {
+        return depth(root);
+    }
+
+    private int depth(Node<T> node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(depth(node.getLeft()), depth(node.getRight()));
+    }
+
+    private Node<T> fromList(List<T> list) {
+        int start = 0;
+        int last = list.size()-1;
+        this.root = fromList(list, start, last);
+        return root;
+    }
+
+    private Node<T> fromList(List<T> list, int start, int last) {
+        int mid = (start + last) / 2;
+        if (start > last) {
+            return null;
+        }
+            Node<T> ret = new Node<>();
+            ret.setValue(list.get(mid));
+            ret.setLeft(fromList(list,start,mid-1));
+            ret.setRight(fromList(list,mid+1,last));
+            return ret;
+        }
+
+        public Node<T> reBalance(){
+        Node<T> reb = fromList(this.listAll().stream().map(Node::getValue).toList());
+        this.root =reb;
+        return reb;
+        }
 
 }
--- src/tree/TestBst.java
+++ src/tree/TestBst.java
@@ -2,15 +2,25 @@ package tree;
 
 import core.Node;
 
+import java.util.List;
+
 public class TestBst {
     public static void main(String[] args) {
-        BSTree<String> my = new BSTree<>();
-        my.add("g");
-        my.add("ga");
-        my.add("gar");
-        my.add("ad");
-        my.add("aer");
-        my.listAll().stream().map(Node::getValue).forEach(System.out::println);
 
+
+        BSTree<String> newy=  new BSTree<>();
+        newy.add("a");
+        newy.add("b");
+        newy.add("c");
+        newy.add("d");
+        newy.add("e");
+        newy.add("f");
+        newy.add("g");
+
+        newy.listAll().stream().map(Node::getValue).forEach(System.out::println);
+        System.out.println(newy.depth());
+        newy.reBalance();
+        newy.listAll().stream().map(Node::getValue).forEach(System.out::println);
+        System.out.println(newy.depth());
     }
 }
